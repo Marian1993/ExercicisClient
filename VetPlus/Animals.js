c5d3f4fcdc1ed3647fe54d895e308cf6a31e05d1
DESKTOP-IG4JCL5\maria <marianmartorell.93@gmail.com>
2023-11-03 13:57:12 +0100

    fer lo d'es animals

--- VetPlus/Animals.js
+++ VetPlus/Animals.js
@@ -1,4 +1,21 @@
-function Hola() {
-    alert("Hola");
+
+(function getAnimals() {
+    fetch('https://theteacher.codiblau.com/public/vetplus/getAnimals')
+        .then(function (resposta){
+            return resposta.json();
+        }).then(function (animals){
+            const taula = document.querySelector("#llista");
+
+        for (let i = 0; i < animals.length; i++) {
+
+           console.log(animals[i]);
+        }
+
+    })
+})();
+function getAnimal() {
+    
+}
+function save() {
 
 }
