633a466700f34b02744bed4fd45b980bb382a289
Maria Antonia <marianmartorell.93@gmail.com>
2023-11-13 20:41:50 +0100

    res

--- VetPlus/Animals.js
+++ VetPlus/Animals.js
@@ -1,25 +1,34 @@
 
-(function getAnimals() {
-    fetch('https://theteacher.codiblau.com/public/vetplus/getAnimals')
+const url = "https://theteacher.codiblau.com/public/vetplus/";
+
+if(this.window.location.pathname === "/ExercicisClient/VetPlus/animals.html"){
+    getAnimals();
+}
+function getAnimals() {
+    fetch(url + 'getAnimals')
         .then(function (resposta){
             return resposta.json();
         }).then(function (animals){
             const taula = document.querySelector("#llista");
 
+            for (let i = 0; i < animals.length; i++) {
 
-        for (let i = 0; i < animals.length; i++) {
-
-            let filaAnimal = '<tr>';
-            filaAnimal += `<td>${animals[i].nom}</td>`;
-            filaAnimal += '<td><a href="animalForm.html">Editar</a></td>';
-            filaAnimal += '</tr>';
-            taula.innerHTML += filaAnimal;
-        }
+                let idHref = "animalForm.html?id=" + animals[i].idanimal;
+                let filaAnimal = '<tr>';
+                filaAnimal += `<td>${animals[i].nom}</td>`;
+                filaAnimal += `<td><a href=${idHref} onclick="getAnimal(function (){return ${animals[i]}})">Editar</a></td>`;
+                filaAnimal += '</tr>';
+                taula.innerHTML += filaAnimal;
+            }
 
     })
-})();
-function getAnimal() {
-    
+}
+function getAnimal(animal) {
+    console.log("Entra");
+    const url = new URL(window.location.href);
+    const id = url.searchParams.get("id");
+    console.log(animal);
+    console.log(id);
 }
 function save() {
 
