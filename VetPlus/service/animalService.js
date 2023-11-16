9ba7bb267882704cc4fa92bbcbcf6679565f0132
Maria Antonia <marianmartorell.93@gmail.com>
2023-11-16 22:04:12 +0100

    error save

--- VetPlus/service/animalService.js
+++ VetPlus/service/animalService.js
@@ -2,20 +2,6 @@
 import {Animal} from "../model/animal.js";
 export const urlJson = "https://theteacher.codiblau.com/public/vetplus/";
 
-export function elegirAnimal(){
-    document.querySelector("#tipus").addEventListener('change',(ev) =>{
-        if(ev.target.value === 'ca'){
-            document.querySelector("#federat").style.display = "none";
-            document.querySelector("#registre").style.display = "block";
-        }else if(ev.target.value === 'cavall'){
-            document.querySelector("#registre").style.display = "none";
-            document.querySelector("#federat").style.display = "block";
-        }else if(ev.target.value === 'moix'){
-            document.querySelector("#registre").style.display = "none";
-            document.querySelector("#federat").style.display = "none";
-        }
-    })
-}
 
 export async function getAnimals() {
     const peticio = await fetch(urlJson + 'getAnimals')
@@ -32,29 +18,23 @@ function jsonToAnimal(json){
     return new Animal(json.idanimal, json.nom, json.sexe, json.numregistre, json.tipus);
 }
 
-export async function getAnimal() {
-
-    const url = new URL(window.location.href);
-    const id = url.searchParams.get("id");
+export async function getAnimal(id) {
 
     const peticio = await fetch(urlJson + 'getAnimals')
 
             const animals = await peticio.json();
-console.log(animals);
             const animal = animals.find(({idanimal}) => idanimal == id);
-            console.log(id);
-            console.log(animal);
 
             return animal;
-
-
-
-
-
 }
 
-async function save(nom,sexe,numregistre,tipus) {
-    const saveFetch = await fetch(this.urlJson + 'save',{
+ export async function save(nom,sexe,numregistre,tipus) {
+
+    console.log(nom);
+    console.log(sexe);
+    console.log(numregistre);
+    console.log(tipus);
+    /*const saveFetch = await fetch(this.urlJson + 'save',{
         method: 'post',
         headers:{
             'Content-Type': 'application/json'
@@ -72,4 +52,6 @@ async function save(nom,sexe,numregistre,tipus) {
 
     const saved = await saveFetch.json();
     console.log(saved);
+
+     */
 }
